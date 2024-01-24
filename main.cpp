7e1168ca56fff36f81e6bcc17194af5e9c05334e
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2024-01-24 19:56:10 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include <thread>
-#include <chrono>
+#include <string>
+#include <cstring>
+#include <ctime>
 
-using namespace std;
-using namespace chrono;
+char grid[] = {
+    ' ', ' ', ' ',
+    ' ', ' ', ' ',
+    ' ', ' ', ' '
+};
 
 int patterns[] = {
     // Horizontal
@@ -21,53 +25,18 @@ int patterns[] = {
     2, 4, 6
 };
 
-void insertSymbol(int input)
-{
-    // printf("\nSymbol: %s\n", input);
+struct {
+    char name[64];
+    char sym;
 
-    // switch (input)
-    // {
-    //     case 0:
-    //         grid[0][0] = 'X';
-    //         break;
-    //     case 1:
-    //         grid[0][2] = 'X';
-    //         break;
-    //     case 2:
-    //         grid[0][4] = 'X';
-    //         break;
-    //     case 3:
-    //         grid[2][0] = 'X';
-    //         break;
-    //     case 4:
-    //         grid[2][2] = 'X';
-    //         break;
-    //     case 5:
-    //         grid[2][4] = 'X';
-    //         break;
-    //     case 6:
-    //         grid[4][0] = 'X';
-    //         break;
-    //     case 7:
-    //         grid[4][2] = 'X';
-    //         break;
-    //     case 8:
-    //         grid[4][4] = 'X';
-    //         break;
-    //     default:
-    //         printf("Invalid input. Only 0 to 8\n");
-    //         break;
-    // }
-}
+    void init(const char *name, unsigned int choice) {
+        strcpy(this->name, name);
+        this->sym = (choice % 2 == 0) ? 'X' : 'O';
+    }
+} player, cpu;
 
 int main()
 {
-    char grid[] = {
-        '0', '1', '2',
-        '3', '4', '5',
-        '6', '7', '8'
-    };
-
     int gridLength = (sizeof(grid) / sizeof(char));
 
     unsigned int input;
@@ -81,37 +50,84 @@ int main()
 
     if (input == 0)
     {
+        printf("\nExiting...");
+
         return 0;
     }
     else if (input == 1)
     {
+        srand(time(0));
+
+        // Initialize
+        char playerName[] = "";
+        int randPick = rand();
+
+        printf("\033[2J");
+        printf("Insert your name\n>> ");
+        scanf("%s", playerName);
+
+        player.init(playerName, randPick);
+        cpu.init("CPU", randPick + 1);
+
+        printf("\033[2J");
+        printf(
+            "Hello %s\n"
+            "Your symbol is '%c'\n",
+            player.name,
+            player.sym
+        );
+        
+        
+        while (true) {
+            static clock_t time;
+            time = clock();
+
+            if (time % 3000 == 0) 
+                break;
+        }
+        
+
+        // Gameplay loop
         bool quit = false;
         while (!quit)
         {
-            printf("\033[2J");
+            char *strEmptyCol;
 
+            printf("\033[2J");
             for (int i = 1; i <= gridLength; i++)
             {
                 printf("|%c", grid[i - 1]);
 
+                if (grid[i - 1] == ' ') 
+                {
+                    strcat(strEmptyCol, std::to_string(i).c_str());
+                }
+
                 if (i % 3 == 0)
                 {
                     printf("|\n");
+
+                    for (int j = 0; j < 7; j++) 
+                    {
+                        putchar('-');
+                    }
+
+                    printf("\n");
                 }
             }
+
+            printf("Empty column(s): %s\n\n", strEmptyCol);
 
             printf("Your turn\n>> ");
             scanf("%d", &input);
 
-            insertSymbol(input);
+            
         }
     }
     else
     {
         printf("Invalid input");
     }
-
-    printf("Exiting...");
-
+    
     return 0;
 }
